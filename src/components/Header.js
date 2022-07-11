55f8fba420871464fca170727e3652ac0a22d94a
David-Neto <davidrmachado@gmail.com>
2022-07-10 23:31:08 -0300

    Entrega do requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Loading from '../pages/Loading';
 import { getUser } from '../services/userAPI';
 
@@ -22,14 +23,24 @@ class Header extends Component {
       isLoading: false,
       user: user.name,
     });
-  }
+  };
 
   render() {
     const { user, isLoading } = this.state;
     return (
       <header data-testid="header-component">
-        {isLoading ? <Loading />
-          : (<h1 data-testid="header-user-name">{ user }</h1>)}
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <>
+            <h1 data-testid="header-user-name">{user}</h1>
+            <nav>
+              <Link data-testid="link-to-search" to="/search">Pesquisar</Link>
+              <Link data-testid="link-to-favorites" to="/favorites">Favoritas</Link>
+              <Link data-testid="link-to-profile" to="/profile">Meu perfil</Link>
+            </nav>
+          </>
+        )}
       </header>
     );
   }
