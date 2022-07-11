609040615fbd7e6abea52bcb63dbfe362c73aa6b
David-Neto <davidrmachado@gmail.com>
2022-07-11 00:18:06 -0300

    Entrega do requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,41 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchParam: '',
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { value } = target;
+    this.setState({
+      searchParam: value,
+    });
+  }
+
   render() {
+    const { searchParam } = this.state;
+    const minChar = 2;
     return (
       <div data-testid="page-search">
         <Header />
-        Busca
+        <input
+          data-testid="search-artist-input"
+          type="text"
+          name="searchParam"
+          value={ searchParam }
+          onChange={ this.handleChange }
+        />
+        <button
+          data-testid="search-artist-button"
+          type="submit"
+          disabled={ searchParam.length < minChar }
+        >
+          Buscar
+        </button>
       </div>
     );
   }
